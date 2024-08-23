2a7e135a754182c5476bac018747bc78f17a9211
Saad <imran.saad50@gmail.com>
2024-08-23 15:30:51 +0530

    Created Job Card Component

--- src/pages/JobListing.jsx
+++ src/pages/JobListing.jsx
@@ -1,21 +1,59 @@
 import { getJobs } from "@/api/apiJobs";
+import JobCard from "@/components/JobCard";
 import useFetch from "@/hooks/useFetch";
 import { useUser } from "@clerk/clerk-react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { BarLoader } from "react-spinners";
 
 const JobListing = () => {
   const { isLoaded } = useUser();
 
-  const { fn: fnJobs, data: dataJobs, loading: loadingJobs } = useFetch(getJobs, {});
+  const [searchQuery, setSearchQuery] = useState("");
+  const [location, setLocation] = useState("");
+  const [company_id, setCompany_id] = useState("");
 
-  console.log(dataJobs);
+  const {
+    fn: fnJobs,
+    data: jobs,
+    loading: loadingJobs,
+  } = useFetch(getJobs, { location, company_id, searchQuery });
 
   useEffect(() => {
     if (isLoaded) fnJobs();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isLoaded]);
+  }, [isLoaded, location, company_id, searchQuery]);
 
-  return <div className=''>JobListing</div>;
+  const clearFilters = () => {
+    setSearchQuery("");
+    setCompany_id("");
+    setLocation("");
+  };
+
+  if (!isLoaded) {
+    return <BarLoader className='mb-4' width={"100%"} color='#36d7b7' />;
+  }
+
+  return (
+    <div className=''>
+      <h1 className='gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8'>
+        Latest Jobs
+      </h1>
+
+      {loadingJobs && <BarLoader className='mt-4' width={"100%"} color='#36d7b7' />}
+
+      {loadingJobs === false && (
+        <div className='mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-4'>
+          {jobs?.length ? (
+            jobs.map((job) => {
+              return <JobCard key={job.id} job={job} />;
+            })
+          ) : (
+            <div>NO Jobs Found </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default JobListing;
